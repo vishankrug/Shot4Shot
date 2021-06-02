ac6d5f8b42b753cced3869ea9d06d13306ba9c7e
Vishank Rughwani <vishankr@uw.edu>
2021-06-01 17:01:55 -0700

    initial commit

---
+++ Pods/Target Support Files/Pods-Shot4Shot-Shot4ShotUITests/Pods-Shot4Shot-Shot4ShotUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shot4Shot_Shot4ShotUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Shot4Shot_Shot4ShotUITests
+@end
---
+++ Pods/Target Support Files/Pods-Shot4ShotTests/Pods-Shot4ShotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shot4ShotTests : NSObject
+@end
+@implementation PodsDummy_Pods_Shot4ShotTests
+@end
